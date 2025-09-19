db56ca3afeeebc6aba821ede1c74e7666a12fbe9
omer.habib <omer.habib26@gmail.com>
2025-09-20 04:48:12 +0500

    Created an App with Navigation Drawer

--- App.js
+++ App.js
@@ -1,53 +1,42 @@
+import { Ionicons } from "@expo/vector-icons";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet } from "react-native";
-import CategoriesScreen from "./screens/CategoriesScreen";
-import MealDetailScreen from "./screens/MealDetailScreen";
-import MealsOverviewScreen from "./screens/MealsOverviewScreen";
 
-const Stack = createNativeStackNavigator();
+import UserScreen from "./screens/UserScreen";
+import WelcomeScreen from "./screens/WelcomeScreen";
 
 export default function App() {
+  const Drawer = createDrawerNavigator();
   return (
-    <>
-      <StatusBar style="light" translucent={false} backgroundColor="#351401" />
-      <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={{
-            headerStyle: { backgroundColor: "#351401" },
+    <NavigationContainer>
+      <Drawer.Navigator>
+        <Drawer.Screen
+          name="Welcome"
+          component={WelcomeScreen}
+          options={{
+            headerStyle: { backgroundColor: "#3c0a6b" },
             headerTintColor: "white",
-            contentStyle: {
-              backgroundColor: "#4c3629ff",
-            },
+            title: "Welcome!",
+            drawerLabel: "Welcome Drawer",
+            drawerIcon: ({ color, size }) => (
+              <Ionicons name="home" color={color} size={size} />
+            ),
           }}
-        >
-          <Stack.Screen
-            name="MealsCategories"
-            component={CategoriesScreen}
-            options={{
-              title: "All Categories",
-              headerStyle: { backgroundColor: "#351401" },
-              headerTintColor: "white",
-              contentStyle: {
-                backgroundColor: "#4c3629ff",
-              },
-            }}
-          />
-          <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
-          <Stack.Screen name="MealDetail" component={MealDetailScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </>
+        />
+        <Drawer.Screen
+          name="User"
+          component={UserScreen}
+          options={{
+            headerStyle: { backgroundColor: "#3c0a6b" },
+            headerTintColor: "white",
+            title: "User!",
+            drawerLabel: "User Drawer",
+            drawerIcon: ({ color, size }) => (
+              <Ionicons name="person" color={color} size={size} />
+            ),
+          }}
+        />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  rootScreen: {
-    flex: 1,
-    marginTop: 16,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ screens/UserScreen.js
@@ -0,0 +1,29 @@
+import { Button, StyleSheet, Text, View } from "react-native";
+
+function UserScreen({ navigation }) {
+  function onPressHandler() {
+    navigation.toggleDrawer();
+  }
+  return (
+    <View style={styles.rootContainer}>
+      <Text>
+        This is the <Text style={styles.highlight}>"User"</Text> screen!
+      </Text>
+      <Button title="Open Drawer" onPress={onPressHandler} />
+    </View>
+  );
+}
+
+export default UserScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  highlight: {
+    fontWeight: "bold",
+    color: "#eb1064",
+  },
+});
